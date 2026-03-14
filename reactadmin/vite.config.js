730e39b914add2d8187e42ba6d39d9ef39f81bf4
lokesh-borse <borselokesh81@gmail.com>
2026-03-15 02:17:49 +0530

    Add domain hosts to Vite allowedHosts

--- reactadmin/vite.config.js
+++ reactadmin/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    middlewareMode: true,
+    allowedHosts: ['heymilkmanadmin.duckdns.org', 'www.heymilkmanadmin.duckdns.org', '127.0.0.1', 'localhost']
+  }
 })
--- reactuser/vite.config.js
+++ reactuser/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    middlewareMode: true,
+    allowedHosts: ['heymilkman.duckdns.org', 'www.heymilkman.duckdns.org', '127.0.0.1', 'localhost']
+  }
 })
 
